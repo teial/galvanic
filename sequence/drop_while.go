eb3462a74406fa2ef4227e6d8ba0b48a20c790dc
Teia Leusten <teia.leusten@proton.me>
2024-07-03 14:35:29 +0300

    Add reverse to DropWhile

--- sequence/drop_while.go
+++ sequence/drop_while.go
@@ -1,28 +1,46 @@
 package sequence
 
-func (seq Sequence[E]) DropWhile(p func(E) bool) Sequence[E] {
-	return Sequence[E]{
-		func(yield func(E) bool) {
-			dropOver := false
-			for e := range seq.Fn {
-				dropOver = dropOver || !p(e)
-				if dropOver && !yield(e) {
+import "iter"
+
+func makeDropWhileFn[E any](p func(E) bool, source iter.Seq[E]) iter.Seq[E] {
+	return func(yield func(E) bool) {
+		dropOver := false
+		for e := range source {
+			dropOver = dropOver || !p(e)
+			if dropOver && !yield(e) {
+				return
+			}
+		}
+	}
+}
+
+func makeDropWhileFn2[E any](p func(E) bool, source iter.Seq[E]) iter.Seq2[int, E] {
+	return func(yield func(int, E) bool) {
+		dropOver := false
+		i := 0
+		for e := range source {
+			dropOver = dropOver || !p(e)
+			if dropOver {
+				if !yield(i, e) {
 					return
 				}
+				i++
 			}
-		},
-		func(yield func(int, E) bool) {
-			dropOver := false
-			i := 0
-			for e := range seq.Fn {
-				dropOver = dropOver || !p(e)
-				if dropOver {
-					if !yield(i, e) {
-						return
-					}
-					i++
-				}
-			}
-		},
+		}
+	}
+}
+
+func (seq Sequence[E]) DropWhile(p func(E) bool) Sequence[E] {
+	return Sequence[E]{
+		makeDropWhileFn(p, seq.Fn),
+		makeDropWhileFn2(p, seq.Fn),
+	}
+}
+
+func (rseq ReversibleSequence[E]) DropWhile(p func(E) bool) ReversibleSequence[E] {
+	return ReversibleSequence[E]{
+		rseq.Sequence.DropWhile(p),
+		makeDropWhileFn(p, rseq.RevFn),
+		makeDropWhileFn2(p, rseq.RevFn),
 	}
 }
--- sequence/drop_while_test.go
+++ sequence/drop_while_test.go
@@ -60,10 +60,76 @@ func TestDropWhile(t *testing.T) {
 	}
 }
 
+func TestDropWhile_Reverse(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name string
+		seq  ReverseIterable[int]
+		fn   func(int) bool
+		want []int
+	}{
+		{
+			name: "empty",
+			seq:  Empty[int](),
+			fn:   func(int) bool { return true },
+			want: []int{},
+		},
+		{
+			name: "all true",
+			seq:  Values(1, 2, 3, 4, 5),
+			fn:   func(int) bool { return true },
+			want: []int{},
+		},
+		{
+			name: "all false",
+			seq:  Values(1, 2, 3, 4, 5),
+			fn:   func(int) bool { return false },
+			want: []int{5, 4, 3, 2, 1},
+		},
+		{
+			name: "some true",
+			seq:  Values(1, 2, 3, 4, 5),
+			fn:   func(i int) bool { return i > 3 },
+			want: []int{3, 2, 1},
+		},
+		{
+			name: "some false",
+			seq:  Values(1, 2, 3, 4, 5),
+			fn:   func(i int) bool { return i < 3 },
+			want: []int{5, 4, 3, 2, 1},
+		},
+		{
+			name: "mixed",
+			seq:  Values(1, 3, 6, 5, 2, 4),
+			fn:   func(i int) bool { return i%2 == 0 },
+			want: []int{5, 6, 3, 1},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			slice := tc.seq.Reverse().DropWhile(tc.fn).Collect()
+			assert.Equal(t, tc.want, slice, "Expected %v, got %v", tc.want, slice)
+		})
+	}
+}
+
 func TestDropWhile_Indexes(t *testing.T) {
 	t.Parallel()
 	indexes := make([]int, 0)
 	sequence := Values(2, 4, 1, 3, 5).DropWhile(func(e int) bool { return e%2 == 0 })
+	for i, e := range sequence.Fn2 {
+		_ = e
+		indexes = append(indexes, i)
+	}
+	want := []int{0, 1, 2}
+	assert.Equal(t, want, indexes, "Expected %v, got %v", want, indexes)
+}
+
+func TestDropWhile_Indexes_Reverse(t *testing.T) {
+	t.Parallel()
+	indexes := make([]int, 0)
+	sequence := Values(1, 3, 5, 2, 4).Reverse().DropWhile(func(e int) bool { return e%2 == 0 })
 	for i, e := range sequence.Fn2 {
 		_ = e
 		indexes = append(indexes, i)
