2425a919f1ae192af958ce6aa67aeb5410f542e2
Teia Leusten <teia.leusten@proton.me>
2024-07-03 20:53:58 +0300

    Implement reverse for Chain

--- sequence/chain.go
+++ sequence/chain.go
@@ -35,3 +35,88 @@ func (seq Sequence[E]) Chain(seqs ...Iterable[E]) Sequence[E] {
 		},
 	}
 }
+
+func (seq ReversibleSequence[E]) Chain(seqs ...ReverseIterable[E]) ReverseIterable[E] {
+	end := len(seqs) - 1
+	return ReversibleSequence[E]{
+		Sequence[E]{
+			func(yield func(E) bool) {
+				for e := range seq.Fn {
+					if !yield(e) {
+						return
+					}
+				}
+				for _, seq := range seqs {
+					for e := range seq.All().Fn {
+						if !yield(e) {
+							return
+						}
+					}
+				}
+			},
+			func(yield func(int, E) bool) {
+				i := 0
+				for _, e := range seq.Fn2 {
+					if !yield(i, e) {
+						return
+					}
+					i++
+				}
+				for _, seq := range seqs {
+					for _, e := range seq.All().Fn2 {
+						if !yield(i, e) {
+							return
+						}
+						i++
+					}
+				}
+			},
+		},
+		func(yield func(E) bool) {
+			if end >= 0 {
+				for e := range seqs[end].Reverse().Fn {
+					if !yield(e) {
+						return
+					}
+				}
+				for i := end - 1; i >= 0; i-- {
+					for e := range seqs[i].Reverse().Fn {
+						if !yield(e) {
+							return
+						}
+					}
+				}
+			}
+			for e := range seq.RevFn {
+				if !yield(e) {
+					return
+				}
+			}
+		},
+		func(yield func(int, E) bool) {
+			i := 0
+			if end >= 0 {
+				for e := range seqs[end].Reverse().Fn {
+					if !yield(i, e) {
+						return
+					}
+					i++
+				}
+				for i := end - 1; i >= 0; i-- {
+					for e := range seqs[i].Reverse().Fn {
+						if !yield(i, e) {
+							return
+						}
+					}
+					i++
+				}
+			}
+			for e := range seq.RevFn {
+				if !yield(i, e) {
+					return
+				}
+				i++
+			}
+		},
+	}
+}
--- sequence/chain_test.go
+++ sequence/chain_test.go
@@ -114,6 +114,114 @@ func TestChain(t *testing.T) {
 	}
 }
 
+func TestChain_Reverse(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name  string
+		first ReverseIterable[int]
+		seqs  []ReverseIterable[int]
+		want  []int
+	}{
+		{
+			name:  "first empty seqs empty",
+			first: Empty[int](),
+			seqs:  []ReverseIterable[int]{},
+			want:  []int{},
+		},
+		{
+			name:  "first empty seqs single",
+			first: Empty[int](),
+			seqs:  []ReverseIterable[int]{Values(1)},
+			want:  []int{1},
+		},
+		{
+			name:  "first empty seqs multiple",
+			first: Empty[int](),
+			seqs:  []ReverseIterable[int]{Values(1, 2, 3)},
+			want:  []int{3, 2, 1},
+		},
+		{
+			name:  "first empty seqs mixed",
+			first: Empty[int](),
+			seqs: []ReverseIterable[int]{
+				Empty[int](),
+				Values(1),
+				Empty[int](),
+				Values(2, 3),
+				Empty[int](),
+			},
+			want: []int{3, 2, 1},
+		},
+		{
+			name:  "first single seqs empty",
+			first: Values(1),
+			seqs:  []ReverseIterable[int]{},
+			want:  []int{1},
+		},
+		{
+			name:  "first single seqs single",
+			first: Values(1),
+			seqs:  []ReverseIterable[int]{Values(2)},
+			want:  []int{2, 1},
+		},
+		{
+			name:  "first single seqs multiple",
+			first: Values(1),
+			seqs:  []ReverseIterable[int]{Values(2, 3, 4)},
+			want:  []int{4, 3, 2, 1},
+		},
+		{
+			name:  "first single seqs mixed",
+			first: Values(1),
+			seqs: []ReverseIterable[int]{
+				Empty[int](),
+				Values(2),
+				Empty[int](),
+				Values(3, 4),
+				Empty[int](),
+			},
+			want: []int{4, 3, 2, 1},
+		},
+		{
+			name:  "first multiple seqs empty",
+			first: Values(1, 2, 3),
+			seqs:  []ReverseIterable[int]{},
+			want:  []int{3, 2, 1},
+		},
+		{
+			name:  "first multiple seqs single",
+			first: Values(1, 2, 3),
+			seqs:  []ReverseIterable[int]{Values(4)},
+			want:  []int{4, 3, 2, 1},
+		},
+		{
+			name:  "first multiple seqs multiple",
+			first: Values(1, 2, 3),
+			seqs:  []ReverseIterable[int]{Values(4, 5, 6)},
+			want:  []int{6, 5, 4, 3, 2, 1},
+		},
+		{
+			name:  "first multiple seqs mixed",
+			first: Values(1, 2, 3),
+			seqs: []ReverseIterable[int]{
+				Empty[int](),
+				Values(4),
+				Empty[int](),
+				Values(5, 6),
+				Empty[int](),
+			},
+			want: []int{6, 5, 4, 3, 2, 1},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			slice := tc.first.AllR().Chain(tc.seqs...).Reverse().Collect()
+			assert.Equal(t, tc.want, slice, "Expected %v, got %v", tc.want, slice)
+		})
+	}
+}
+
 func TestChain_Indexes(t *testing.T) {
 	t.Parallel()
 	indexes := make([]int, 0)
@@ -121,7 +229,7 @@ func TestChain_Indexes(t *testing.T) {
 		1,
 		2,
 	).Chain(Empty[int](), Values(3), Empty[int](), Values(4, 5), Empty[int]())
-	for i, e := range sequence.Fn2 {
+	for i, e := range sequence.All().Fn2 {
 		_ = e
 		indexes = append(indexes, i)
 	}
