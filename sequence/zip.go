e1dad4fb81ec7cdb5aa0530cf4830804a0b6d7f8
Teia Leusten <teia.leusten@proton.me>
2024-07-03 16:23:10 +0300

    Add reverse to Zip

--- sequence/zip.go
+++ sequence/zip.go
@@ -13,35 +13,51 @@ func NewPair[S, T any](v1 S, v2 T) Pair[S, T] {
 	return Pair[S, T]{V1: v1, OK1: true, V2: v2, OK2: true}
 }
 
+func makeZipFn[S, T any](s iter.Seq[S], t iter.Seq[T]) iter.Seq[Pair[S, T]] {
+	return func(yield func(Pair[S, T]) bool) {
+		pull1, stop := iter.Pull(s)
+		defer stop()
+		pull2, stop := iter.Pull(t)
+		defer stop()
+		for {
+			var pair Pair[S, T]
+			pair.V1, pair.OK1 = pull1()
+			pair.V2, pair.OK2 = pull2()
+			if !pair.OK1 || !pair.OK2 || !yield(pair) {
+				return
+			}
+		}
+	}
+}
+
+func makeZipFn2[S, T any](s iter.Seq[S], t iter.Seq[T]) iter.Seq2[int, Pair[S, T]] {
+	return func(yield func(int, Pair[S, T]) bool) {
+		pull1, stop := iter.Pull(s)
+		defer stop()
+		pull2, stop := iter.Pull(t)
+		defer stop()
+		for i := 0; ; i++ {
+			var pair Pair[S, T]
+			pair.V1, pair.OK1 = pull1()
+			pair.V2, pair.OK2 = pull2()
+			if !pair.OK1 || !pair.OK2 || !yield(i, pair) {
+				return
+			}
+		}
+	}
+}
+
 func Zip[S, T any](s Iterable[S], t Iterable[T]) Sequence[Pair[S, T]] {
 	return Sequence[Pair[S, T]]{
-		func(yield func(Pair[S, T]) bool) {
-			pull1, stop := iter.Pull(s.All().Fn)
-			defer stop()
-			pull2, stop := iter.Pull(t.All().Fn)
-			defer stop()
-			for {
-				var pair Pair[S, T]
-				pair.V1, pair.OK1 = pull1()
-				pair.V2, pair.OK2 = pull2()
-				if !pair.OK1 || !pair.OK2 || !yield(pair) {
-					return
-				}
-			}
-		},
-		func(yield func(int, Pair[S, T]) bool) {
-			pull1, stop := iter.Pull(s.All().Fn)
-			defer stop()
-			pull2, stop := iter.Pull(t.All().Fn)
-			defer stop()
-			for i := 0; ; i++ {
-				var pair Pair[S, T]
-				pair.V1, pair.OK1 = pull1()
-				pair.V2, pair.OK2 = pull2()
-				if !pair.OK1 || !pair.OK2 || !yield(i, pair) {
-					return
-				}
-			}
-		},
+		makeZipFn(s.All().Fn, t.All().Fn),
+		makeZipFn2(s.All().Fn, t.All().Fn),
+	}
+}
+
+func ZipR[S, T any](s ReverseIterable[S], t ReverseIterable[T]) ReversibleSequence[Pair[S, T]] {
+	return ReversibleSequence[Pair[S, T]]{
+		Zip[S, T](s, t),
+		makeZipFn(s.Reverse().Fn, t.Reverse().Fn),
+		makeZipFn2(s.Reverse().Fn, t.Reverse().Fn),
 	}
 }
--- sequence/zip_test.go
+++ sequence/zip_test.go
@@ -66,12 +66,205 @@ func TestZip(t *testing.T) {
 	}
 }
 
+func TestZip_ReverseInputs(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name string
+		seq1 ReverseIterable[int]
+		seq2 ReverseIterable[int]
+		want []Pair[int, int]
+	}{
+		{
+			name: "empty",
+			seq1: Empty[int](),
+			seq2: Empty[int](),
+			want: []Pair[int, int]{},
+		},
+		{
+			name: "single",
+			seq1: Values(1),
+			seq2: Values(2),
+			want: []Pair[int, int]{NewPair(1, 2)},
+		},
+		{
+			name: "multiple",
+			seq1: Values(1, 2, 3),
+			seq2: Values(4, 5, 6),
+			want: []Pair[int, int]{NewPair(3, 6), NewPair(2, 5), NewPair(1, 4)},
+		},
+		{
+			name: "different length",
+			seq1: Values(1, 2, 3),
+			seq2: Values(4, 5),
+			want: []Pair[int, int]{NewPair(3, 5), NewPair(2, 4)},
+		},
+		{
+			name: "different length 2",
+			seq1: Values(1, 2),
+			seq2: Values(4, 5, 6),
+			want: []Pair[int, int]{NewPair(2, 6), NewPair(1, 5)},
+		},
+		{
+			name: "different length 3",
+			seq1: Values(1, 2, 3),
+			seq2: Empty[int](),
+			want: []Pair[int, int]{},
+		},
+		{
+			name: "different length 4",
+			seq1: Empty[int](),
+			seq2: Values(4, 5, 6),
+			want: []Pair[int, int]{},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			zipped := ZipR(tc.seq1.Reverse(), tc.seq2.Reverse()).Collect()
+			assert.Equal(t, tc.want, zipped, "Expected %v, got %v", tc.want, zipped)
+		})
+	}
+}
+
+func TestZip_ReverseOutput(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name string
+		seq1 ReverseIterable[int]
+		seq2 ReverseIterable[int]
+		want []Pair[int, int]
+	}{
+		{
+			name: "empty",
+			seq1: Empty[int](),
+			seq2: Empty[int](),
+			want: []Pair[int, int]{},
+		},
+		{
+			name: "single",
+			seq1: Values(1),
+			seq2: Values(2),
+			want: []Pair[int, int]{NewPair(1, 2)},
+		},
+		{
+			name: "multiple",
+			seq1: Values(1, 2, 3),
+			seq2: Values(4, 5, 6),
+			want: []Pair[int, int]{NewPair(3, 6), NewPair(2, 5), NewPair(1, 4)},
+		},
+		{
+			name: "different length",
+			seq1: Values(1, 2, 3),
+			seq2: Values(4, 5),
+			want: []Pair[int, int]{NewPair(3, 5), NewPair(2, 4)},
+		},
+		{
+			name: "different length 2",
+			seq1: Values(1, 2),
+			seq2: Values(4, 5, 6),
+			want: []Pair[int, int]{NewPair(2, 6), NewPair(1, 5)},
+		},
+		{
+			name: "different length 3",
+			seq1: Values(1, 2, 3),
+			seq2: Empty[int](),
+			want: []Pair[int, int]{},
+		},
+		{
+			name: "different length 4",
+			seq1: Empty[int](),
+			seq2: Values(4, 5, 6),
+			want: []Pair[int, int]{},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			zipped := ZipR(tc.seq1, tc.seq2).Reverse().Collect()
+			assert.Equal(t, tc.want, zipped, "Expected %v, got %v", tc.want, zipped)
+		})
+	}
+}
+
+func TestZip_DoubleReverse(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name string
+		seq1 ReverseIterable[int]
+		seq2 ReverseIterable[int]
+		want []Pair[int, int]
+	}{
+		{
+			name: "empty",
+			seq1: Empty[int](),
+			seq2: Empty[int](),
+			want: []Pair[int, int]{},
+		},
+		{
+			name: "single",
+			seq1: Values(1),
+			seq2: Values(2),
+			want: []Pair[int, int]{NewPair(1, 2)},
+		},
+		{
+			name: "multiple",
+			seq1: Values(1, 2, 3),
+			seq2: Values(4, 5, 6),
+			want: []Pair[int, int]{NewPair(1, 4), NewPair(2, 5), NewPair(3, 6)},
+		},
+		{
+			name: "different length",
+			seq1: Values(1, 2, 3),
+			seq2: Values(4, 5),
+			want: []Pair[int, int]{NewPair(1, 4), NewPair(2, 5)},
+		},
+		{
+			name: "different length 2",
+			seq1: Values(1, 2),
+			seq2: Values(4, 5, 6),
+			want: []Pair[int, int]{NewPair(1, 4), NewPair(2, 5)},
+		},
+		{
+			name: "different length 3",
+			seq1: Values(1, 2, 3),
+			seq2: Empty[int](),
+			want: []Pair[int, int]{},
+		},
+		{
+			name: "different length 4",
+			seq1: Empty[int](),
+			seq2: Values(4, 5, 6),
+			want: []Pair[int, int]{},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			zipped := ZipR(tc.seq1.Reverse(), tc.seq2.Reverse()).Reverse().Collect()
+			assert.Equal(t, tc.want, zipped, "Expected %v, got %v", tc.want, zipped)
+		})
+	}
+}
+
 func TestZip_Indexes(t *testing.T) {
 	t.Parallel()
 	indexes := make([]int, 0)
 	slice := Values(1, 2, 3)
 	other := Values(1, 2)
 	for i, e := range Zip(slice, other).Fn2 {
+		_ = e
+		indexes = append(indexes, i)
+	}
+	want := []int{0, 1}
+	assert.Equal(t, want, indexes, "Expected %v, got %v", want, indexes)
+}
+
+func TestZip_Indexes_Reverse(t *testing.T) {
+	t.Parallel()
+	indexes := make([]int, 0)
+	slice := Values(1, 2, 3)
+	other := Values(1, 2)
+	for i, e := range ZipR(slice, other).Reverse().Fn2 {
 		_ = e
 		indexes = append(indexes, i)
 	}
