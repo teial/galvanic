bd6e5c99161f1bdb4663fb0eb9570bbbd5cecfc4
Teia Leusten <teia.leusten@proton.me>
2024-07-03 15:06:05 +0300

    Add reverse to Map

--- sequence/map.go
+++ sequence/map.go
@@ -1,20 +1,38 @@
 package sequence
 
+import "iter"
+
+func makeMapFn[S, T any](f func(S) T, source iter.Seq[S]) iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for e := range source {
+			if !yield(f(e)) {
+				return
+			}
+		}
+	}
+}
+
+func makeMapFn2[S, T any](f func(S) T, source iter.Seq2[int, S]) iter.Seq2[int, T] {
+	return func(yield func(int, T) bool) {
+		for i, e := range source {
+			if !yield(i, f(e)) {
+				return
+			}
+		}
+	}
+}
+
 func Map[S, T any](seq Iterable[S], f func(S) T) Sequence[T] {
 	return Sequence[T]{
-		func(yield func(T) bool) {
-			for e := range seq.All().Fn {
-				if !yield(f(e)) {
-					return
-				}
-			}
-		},
-		func(yield func(int, T) bool) {
-			for i, e := range seq.All().Fn2 {
-				if !yield(i, f(e)) {
-					return
-				}
-			}
-		},
+		makeMapFn(f, seq.All().Fn),
+		makeMapFn2(f, seq.All().Fn2),
+	}
+}
+
+func MapR[S, T any](seq ReverseIterable[S], f func(S) T) ReversibleSequence[T] {
+	return ReversibleSequence[T]{
+		Map[S, T](seq, f),
+		makeMapFn(f, seq.Reverse().Fn),
+		makeMapFn2(f, seq.Reverse().Fn2),
 	}
 }
--- sequence/map_test.go
+++ sequence/map_test.go
@@ -42,11 +42,96 @@ func TestMap(t *testing.T) {
 	}
 }
 
+func TestMap_Reverse(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name string
+		seq  ReverseIterable[string]
+		fn   func(string) int
+		want []int
+	}{
+		{
+			name: "empty",
+			seq:  Empty[string](),
+			fn:   func(string) int { return 0 },
+			want: []int{},
+		},
+		{
+			name: "single",
+			seq:  Values("a"),
+			fn:   func(string) int { return 1 },
+			want: []int{1},
+		},
+		{
+			name: "multiple",
+			seq:  Values("a", "ab", "abc"),
+			fn:   func(s string) int { return len(s) },
+			want: []int{3, 2, 1},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			slice := MapR(tc.seq.Reverse(), tc.fn).Collect()
+			assert.Equal(t, tc.want, slice, "Expected %v, got %v", tc.want, slice)
+		})
+	}
+}
+
+func TestMap_DoubleReverse(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name string
+		seq  ReverseIterable[string]
+		fn   func(string) int
+		want []int
+	}{
+		{
+			name: "empty",
+			seq:  Empty[string](),
+			fn:   func(string) int { return 0 },
+			want: []int{},
+		},
+		{
+			name: "single",
+			seq:  Values("a"),
+			fn:   func(string) int { return 1 },
+			want: []int{1},
+		},
+		{
+			name: "multiple",
+			seq:  Values("a", "ab", "abc"),
+			fn:   func(s string) int { return len(s) },
+			want: []int{1, 2, 3},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			slice := MapR(tc.seq.Reverse(), tc.fn).Reverse().Collect()
+			assert.Equal(t, tc.want, slice, "Expected %v, got %v", tc.want, slice)
+		})
+	}
+}
+
 func TestMap_Indexes(t *testing.T) {
 	t.Parallel()
 	indexes := make([]int, 0)
 	sequence := Values("a", "ab", "abc")
 	mapped := Map(sequence, func(e string) int { return len(e) })
+	for i, e := range mapped.Fn2 {
+		_ = e
+		indexes = append(indexes, i)
+	}
+	want := []int{0, 1, 2}
+	assert.Equal(t, want, indexes, "Expected %v, got %v", want, indexes)
+}
+
+func TestMap_Indexes_Reverse(t *testing.T) {
+	t.Parallel()
+	indexes := make([]int, 0)
+	sequence := Values("a", "ab", "abc").Reverse()
+	mapped := MapR(sequence, func(e string) int { return len(e) })
 	for i, e := range mapped.Fn2 {
 		_ = e
 		indexes = append(indexes, i)
