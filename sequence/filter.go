10049d62bfb3980f489ef73c79607c8448745afa
Teia Leusten <teia.leusten@proton.me>
2024-07-03 14:43:13 +0300

    Add reverse to Filter

--- sequence/filter.go
+++ sequence/filter.go
@@ -1,24 +1,42 @@
 package sequence
 
-func (seq Sequence[E]) Filter(p func(E) bool) Sequence[E] {
-	return Sequence[E]{
-		func(yield func(E) bool) {
-			for e := range seq.Fn {
-				if p(e) && !yield(e) {
+import "iter"
+
+func makeFilterFn[E any](p func(E) bool, source iter.Seq[E]) iter.Seq[E] {
+	return func(yield func(E) bool) {
+		for e := range source {
+			if p(e) && !yield(e) {
+				return
+			}
+		}
+	}
+}
+
+func makeFilterFn2[E any](p func(E) bool, source iter.Seq[E]) iter.Seq2[int, E] {
+	return func(yield func(int, E) bool) {
+		i := 0
+		for e := range source {
+			if p(e) {
+				if !yield(i, e) {
 					return
 				}
+				i++
 			}
-		},
-		func(yield func(int, E) bool) {
-			i := 0
-			for e := range seq.Fn {
-				if p(e) {
-					if !yield(i, e) {
-						return
-					}
-					i++
-				}
-			}
-		},
+		}
+	}
+}
+
+func (seq Sequence[E]) Filter(p func(E) bool) Sequence[E] {
+	return Sequence[E]{
+		makeFilterFn(p, seq.Fn),
+		makeFilterFn2(p, seq.Fn),
+	}
+}
+
+func (rseq ReversibleSequence[E]) Filter(p func(E) bool) ReversibleSequence[E] {
+	return ReversibleSequence[E]{
+		rseq.Sequence.Filter(p),
+		makeFilterFn(p, rseq.RevFn),
+		makeFilterFn2(p, rseq.RevFn),
 	}
 }
--- sequence/filter_test.go
+++ sequence/filter_test.go
@@ -51,9 +51,64 @@ func TestFilter(t *testing.T) {
 			want: []int{1, 3},
 		},
 	}
+
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			slice := tc.seq.All().Filter(tc.fn).Collect()
+			assert.Equal(t, tc.want, slice, "Expected %v, got %v", tc.want, slice)
+		})
+	}
+}
+
+func TestFilter_Reverse(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name string
+		seq  ReverseIterable[int]
+		fn   func(int) bool
+		want []int
+	}{
+		{
+			name: "empty",
+			seq:  Empty[int](),
+			fn:   func(int) bool { return true },
+			want: []int{},
+		},
+		{
+			name: "single",
+			seq:  Values(1),
+			fn:   func(int) bool { return true },
+			want: []int{1},
+		},
+		{
+			name: "single false",
+			seq:  Values(1),
+			fn:   func(int) bool { return false },
+			want: []int{},
+		},
+		{
+			name: "multiple",
+			seq:  Values(1, 2, 3),
+			fn:   func(int) bool { return true },
+			want: []int{3, 2, 1},
+		},
+		{
+			name: "multiple false",
+			seq:  Values(1, 2, 3),
+			fn:   func(int) bool { return false },
+			want: []int{},
+		},
+		{
+			name: "multiple odd",
+			seq:  Values(1, 2, 3),
+			fn:   func(e int) bool { return e%2 == 1 },
+			want: []int{3, 1},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			slice := tc.seq.Reverse().Filter(tc.fn).Collect()
 			assert.Equal(t, tc.want, slice, "Expected %v, got %v", tc.want, slice)
 		})
 	}
@@ -63,6 +118,18 @@ func TestFilter_Indexes(t *testing.T) {
 	t.Parallel()
 	indexes := make([]int, 0)
 	sequence := Values(1, 2, 3, 4, 5).Filter(func(e int) bool { return e%2 == 1 })
+	for i, e := range sequence.Fn2 {
+		_ = e
+		indexes = append(indexes, i)
+	}
+	want := []int{0, 1, 2}
+	assert.Equal(t, want, indexes, "Expected %v, got %v", want, indexes)
+}
+
+func TestFilter_Indexes_Reverse(t *testing.T) {
+	t.Parallel()
+	indexes := make([]int, 0)
+	sequence := Values(1, 2, 3, 4, 5).Reverse().Filter(func(e int) bool { return e%2 == 1 })
 	for i, e := range sequence.Fn2 {
 		_ = e
 		indexes = append(indexes, i)
