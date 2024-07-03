7f0176bb57374a0f0d67d7cab110593b86cbf53f
Teia Leusten <teia.leusten@proton.me>
2024-07-03 13:33:05 +0300

    Add reverse to Drop

--- sequence/drop.go
+++ sequence/drop.go
@@ -1,23 +1,41 @@
 package sequence
 
+import "iter"
+
+func makeDropFn[E any](n int, source iter.Seq2[int, E]) iter.Seq[E] {
+	return func(yield func(E) bool) {
+		for i, e := range source {
+			if i >= n && !yield(e) {
+				return
+			}
+		}
+	}
+}
+
+func makeDropFn2[E any](n int, source iter.Seq2[int, E]) iter.Seq2[int, E] {
+	return func(yield func(int, E) bool) {
+		for i, e := range source {
+			if i >= n && !yield(i-n, e) {
+				return
+			}
+		}
+	}
+}
+
 func (seq Sequence[E]) Drop(n int) Sequence[E] {
 	if n < 0 {
 		panic("n must be non-negative")
 	}
 	return Sequence[E]{
-		func(yield func(E) bool) {
-			for i, e := range seq.Fn2 {
-				if i >= n && !yield(e) {
-					return
-				}
-			}
-		},
-		func(yield func(int, E) bool) {
-			for i, e := range seq.Fn2 {
-				if i >= n && !yield(i-n, e) {
-					return
-				}
-			}
-		},
+		makeDropFn(n, seq.Fn2),
+		makeDropFn2(n, seq.Fn2),
+	}
+}
+
+func (rseq ReversibleSequence[E]) Drop(n int) ReversibleSequence[E] {
+	return ReversibleSequence[E]{
+		rseq.Sequence.Drop(n),
+		makeDropFn(n, rseq.RevFn2),
+		makeDropFn2(n, rseq.RevFn2),
 	}
 }
--- sequence/drop_test.go
+++ sequence/drop_test.go
@@ -66,10 +66,81 @@ func TestDrop(t *testing.T) {
 	}
 }
 
+func TestDrop_Reverse(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name string
+		seq  ReverseIterable[int]
+		n    int
+		want []int
+	}{
+		{
+			name: "empty",
+			seq:  Empty[int](),
+			n:    0,
+			want: []int{},
+		},
+		{
+			name: "single",
+			seq:  Values(1),
+			n:    1,
+			want: []int{},
+		},
+		{
+			name: "single 0",
+			seq:  Values(1),
+			n:    0,
+			want: []int{1},
+		},
+		{
+			name: "single 2",
+			seq:  Values(1),
+			n:    2,
+			want: []int{},
+		},
+		{
+			name: "multiple",
+			seq:  Values(1, 2, 3),
+			n:    2,
+			want: []int{1},
+		},
+		{
+			name: "multiple 0",
+			seq:  Values(1, 2, 3),
+			n:    0,
+			want: []int{3, 2, 1},
+		},
+		{
+			name: "multiple 4",
+			seq:  Values(1, 2, 3),
+			n:    4,
+			want: []int{},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			slice := tc.seq.Reverse().Drop(tc.n).Collect()
+			assert.Equal(t, tc.want, slice, "Expected %v, got %v", tc.want, slice)
+		})
+	}
+}
+
 func TestDrop_Indexes(t *testing.T) {
 	t.Parallel()
 	indexes := make([]int, 0)
 	for i, e := range Values(1, 2, 3, 4, 5).Drop(2).Fn2 {
+		_ = e
+		indexes = append(indexes, i)
+	}
+	want := []int{0, 1, 2}
+	assert.Equal(t, want, indexes, "Expected %v, got %v", want, indexes)
+}
+
+func TestDrop_Indexes_Reverse(t *testing.T) {
+	t.Parallel()
+	indexes := make([]int, 0)
+	for i, e := range Values(1, 2, 3, 4, 5).Reverse().Drop(2).Fn2 {
 		_ = e
 		indexes = append(indexes, i)
 	}
